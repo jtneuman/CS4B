213c4eddefb3c74a32f6165c12fa516a4f7d2d16
Jeff Neumann <Jeff.Neumann@phoenix.edu>
2017-08-23 14:52:12 -0400

    Stack exercise complete.

---
+++ DeckOfCards/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DeckOfCards
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.lstCards = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(22, 23);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 23);
+            this.btnDraw.TabIndex = 0;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // lstCards
+            // 
+            this.lstCards.FormattingEnabled = true;
+            this.lstCards.Location = new System.Drawing.Point(22, 67);
+            this.lstCards.Name = "lstCards";
+            this.lstCards.Size = new System.Drawing.Size(200, 173);
+            this.lstCards.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lstCards);
+            this.Controls.Add(this.btnDraw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.ListBox lstCards;
+    }
+}
+
---
+++ DeckOfCards/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeckOfCards
+{
+    public partial class Form1 : Form
+    {
+
+        enum Suit { Hearts = 1, Spades, Diamonds, Clubs }
+        enum Value { Ace = 1, Two, Three, Four, Five, Six, Seven, Eight,
+            Nine, Ten, Jack, Queen, King }
+
+        struct Card
+        {
+            private Suit _suit;
+            private Value _value;
+
+            public Value Value { get { return _value; } }
+            public Suit Suit { get { return _suit; } }
+
+            public Card(Value value, Suit suit)
+            {
+                _value = value;
+                _suit = suit;
+            }
+        }
+
+        Stack<Card> deck = new Stack<Card>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int value = 1; value <= 13; value++)
+                for (int suit = 1; suit <= 4; suit++)
+                    deck.Push(new Card((Value)value, (Suit)suit));
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            if(deck.Count > 0)
+            {
+                var card = deck.Pop();
+                lstCards.Items.Add(String.Format("{0} of {1}", card.Value,
+                    card.Suit));
+            }
+        }
+    }
+}
