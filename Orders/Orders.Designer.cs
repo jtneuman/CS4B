72bf4a0e3655d3bf47ff3b1e26b78fa3715c4ee5
Jeff Neumann <Jeff.Neumann@phoenix.edu>
2017-08-23 16:50:27 -0400

    Orders project done through btnAdd method - next PONO btn - listview columns not displaying right yet either.

---
+++ Orders/Orders.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Orders
+{
+    partial class Orders
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnAddOrder = new System.Windows.Forms.Button();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.cboCustomers = new System.Windows.Forms.ComboBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblCustomer = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnListOrders = new System.Windows.Forms.Button();
+            this.btnProcessNextOrder = new System.Windows.Forms.Button();
+            this.btnPeekOnNextOrder = new System.Windows.Forms.Button();
+            this.lstOrders = new System.Windows.Forms.ListView();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.colOrderNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colTotal = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colCustomer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(24, 23);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(544, 100);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnAddOrder);
+            this.tabPage1.Controls.Add(this.txtTotal);
+            this.tabPage1.Controls.Add(this.cboCustomers);
+            this.tabPage1.Controls.Add(this.lblTotal);
+            this.tabPage1.Controls.Add(this.lblCustomer);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(536, 74);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "New Order";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnAddOrder
+            // 
+            this.btnAddOrder.Location = new System.Drawing.Point(361, 33);
+            this.btnAddOrder.Name = "btnAddOrder";
+            this.btnAddOrder.Size = new System.Drawing.Size(75, 23);
+            this.btnAddOrder.TabIndex = 4;
+            this.btnAddOrder.Text = "Add Order";
+            this.btnAddOrder.UseVisualStyleBackColor = true;
+            this.btnAddOrder.Click += new System.EventHandler(this.btnAddOrder_Click);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(185, 33);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 3;
+            // 
+            // cboCustomers
+            // 
+            this.cboCustomers.FormattingEnabled = true;
+            this.cboCustomers.Location = new System.Drawing.Point(19, 33);
+            this.cboCustomers.Name = "cboCustomers";
+            this.cboCustomers.Size = new System.Drawing.Size(121, 21);
+            this.cboCustomers.TabIndex = 2;
+            this.cboCustomers.SelectedIndexChanged += new System.EventHandler(this.cboCustomers_SelectedIndexChanged);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(182, 16);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(31, 13);
+            this.lblTotal.TabIndex = 1;
+            this.lblTotal.Text = "Total";
+            // 
+            // lblCustomer
+            // 
+            this.lblCustomer.AutoSize = true;
+            this.lblCustomer.Location = new System.Drawing.Point(16, 16);
+            this.lblCustomer.Name = "lblCustomer";
+            this.lblCustomer.Size = new System.Drawing.Size(51, 13);
+            this.lblCustomer.TabIndex = 0;
+            this.lblCustomer.Text = "Customer";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnListOrders);
+            this.tabPage2.Controls.Add(this.btnProcessNextOrder);
+            this.tabPage2.Controls.Add(this.btnPeekOnNextOrder);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(536, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Manage Orders";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnListOrders
+            // 
+            this.btnListOrders.Location = new System.Drawing.Point(342, 27);
+            this.btnListOrders.Name = "btnListOrders";
+            this.btnListOrders.Size = new System.Drawing.Size(114, 23);
+            this.btnListOrders.TabIndex = 2;
+            this.btnListOrders.Text = "List Orders";
+            this.btnListOrders.UseVisualStyleBackColor = true;
+            // 
+            // btnProcessNextOrder
+            // 
+            this.btnProcessNextOrder.Location = new System.Drawing.Point(175, 27);
+            this.btnProcessNextOrder.Name = "btnProcessNextOrder";
+            this.btnProcessNextOrder.Size = new System.Drawing.Size(120, 23);
+            this.btnProcessNextOrder.TabIndex = 1;
+            this.btnProcessNextOrder.Text = "Process Next Order";
+            this.btnProcessNextOrder.UseVisualStyleBackColor = true;
+            // 
+            // btnPeekOnNextOrder
+            // 
+            this.btnPeekOnNextOrder.Location = new System.Drawing.Point(26, 27);
+            this.btnPeekOnNextOrder.Name = "btnPeekOnNextOrder";
+            this.btnPeekOnNextOrder.Size = new System.Drawing.Size(116, 23);
+            this.btnPeekOnNextOrder.TabIndex = 0;
+            this.btnPeekOnNextOrder.Text = "Peek On Next Order";
+            this.btnPeekOnNextOrder.UseVisualStyleBackColor = true;
+            // 
+            // lstOrders
+            // 
+            this.lstOrders.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colOrderNo,
+            this.colTotal,
+            this.colCustomer});
+            this.lstOrders.Location = new System.Drawing.Point(28, 144);
+            this.lstOrders.Name = "lstOrders";
+            this.lstOrders.Size = new System.Drawing.Size(536, 106);
+            this.lstOrders.TabIndex = 1;
+            this.lstOrders.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(488, 270);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // colOrderNo
+            // 
+            this.colOrderNo.Width = 200;
+            // 
+            // colTotal
+            // 
+            this.colTotal.Width = 100;
+            // 
+            // colCustomer
+            // 
+            this.colCustomer.Width = 150;
+            // 
+            // Orders
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 317);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lstOrders);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Orders";
+            this.Text = "Orders";
+            this.Load += new System.EventHandler(this.Orders_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnAddOrder;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.ComboBox cboCustomers;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblCustomer;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnListOrders;
+        private System.Windows.Forms.Button btnProcessNextOrder;
+        private System.Windows.Forms.Button btnPeekOnNextOrder;
+        private System.Windows.Forms.ListView lstOrders;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ColumnHeader colOrderNo;
+        private System.Windows.Forms.ColumnHeader colTotal;
+        private System.Windows.Forms.ColumnHeader colCustomer;
+    }
+}
+
---
+++ Orders/Orders.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Orders
+{
+    public partial class Orders : Form
+    {
+#region Order struct
+        struct Order
+        {
+            private int _customerId;
+            private double _total;
+            private Guid _orderNo;
+
+            public int CustomerId
+            {
+                get { return _customerId; }
+                set { _customerId = value; }
+            }
+
+            public double Total
+            {
+                get { return _total; }
+                set { _total = value; }
+            }
+
+            public Guid OrderNo
+            {
+                get { return _orderNo; }
+                set { _orderNo = value; }
+            }
+
+            public Order(int customerId, double total, Guid orderNo)
+            {
+                _customerId = customerId;
+                _total = total;
+                _orderNo = orderNo;
+            }
+
+        }
+        #endregion
+#region Customer struct
+        struct Customer
+        {
+            private int _id;
+            private string _name;
+
+            public int Id
+            {
+                get { return _id; }
+                set { _id = value; }
+            }
+
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+
+            public Customer(int id, string name)
+            {
+                _id = id;
+                _name = name;
+            }
+
+        }
+        #endregion
+
+        List<Customer> customers = new List<Customer>();
+        List<Order> fulfilledOrders = new List<Order>();
+        Queue<Order> orders = new Queue<Order>();
+
+        public Orders()
+        {
+            customers.AddRange(new Customer[]
+            {
+                new Customer(1, "John Doe"),
+                new Customer(2, "Stan Smith")
+            });
+
+            InitializeComponent();
+        }
+
+        private void Orders_Load(object sender, EventArgs e)
+        {
+            cboCustomers.DataSource = customers;
+            cboCustomers.DisplayMember = "Name";
+        }
+
+#region Methods
+        private void ShowCustomerOrders(int customerId)
+        {
+            var customer = customers.First(c => c.Id.Equals(customerId));
+            var nextOrderNo = orders.Count > 0 ? orders.Peek().OrderNo : Guid.Empty;
+
+            var items = (from o in orders
+                         where o.CustomerId.Equals(customer.Id)
+                         select new
+                         {
+                             Color = o.OrderNo.Equals(nextOrderNo) ?
+                                Color.LightBlue : Color.LightPink,
+                             Item = new ListViewItem(new string[]
+                             {
+                                 o.OrderNo.ToString(), o.Total.ToString(),
+                                 customer.Name, customer.Id.ToString()
+                             })
+                         }).Union(
+                                from o in fulfilledOrders
+                                where o.CustomerId.Equals(customer.Id)
+                                select new
+                                {
+                                    Color = Color.LightGreen,
+                                    Item = new ListViewItem(new string[]
+                                    {
+                                        o.OrderNo.ToString(), o.Total.ToString(),
+                                        customer.Name, customer.Id.ToString()
+                                    })
+                                });  // End of Union
+
+            lstOrders.Items.Clear();
+            foreach (var item in items)
+            {
+                item.Item.BackColor = item.Color;
+                lstOrders.Items.Add(item.Item);
+            }
+        }
+
+        #endregion
+
+        private void cboCustomers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboCustomers.Items.Count.Equals(0)) return;
+            var customer = (Customer)cboCustomers.SelectedItem;
+            ShowCustomerOrders(customer.Id);
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex.Equals(0))
+            {
+                if (cboCustomers.Items.Count.Equals(0)) return;
+                var customer = (Customer)cboCustomers.SelectedItem;
+                ShowCustomerOrders(customer.Id);
+            }
+        }
+
+        private void btnAddOrder_Click(object sender, EventArgs e)
+        {
+            var total = 0d;
+            var success = Double.TryParse(txtTotal.Text, out total);
+            if (success)
+            {
+                var customerId = ((Customer)cboCustomers.SelectedItem).Id;
+                var order = new Order(customerId, total, Guid.NewGuid());
+                orders.Enqueue(order);
+
+                ShowCustomerOrders(customerId);
+            }
+        }
+    }
+}
