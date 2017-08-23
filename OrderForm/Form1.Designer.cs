39473adb3e76c2bde29e9d1a37183635506df017
Jeff Neumann <Jeff.Neumann@phoenix.edu>
2017-08-23 14:32:45 -0400

    Queue exercise complete

---
+++ OrderForm/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace OrderForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOrderNo = new System.Windows.Forms.Label();
+            this.txtOrderNo = new System.Windows.Forms.TextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.lblInQueue = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.lstOrders = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblOrderNo
+            // 
+            this.lblOrderNo.AutoSize = true;
+            this.lblOrderNo.Location = new System.Drawing.Point(27, 28);
+            this.lblOrderNo.Name = "lblOrderNo";
+            this.lblOrderNo.Size = new System.Drawing.Size(50, 13);
+            this.lblOrderNo.TabIndex = 0;
+            this.lblOrderNo.Text = "OrderNo.";
+            // 
+            // txtOrderNo
+            // 
+            this.txtOrderNo.Location = new System.Drawing.Point(30, 58);
+            this.txtOrderNo.Name = "txtOrderNo";
+            this.txtOrderNo.Size = new System.Drawing.Size(100, 20);
+            this.txtOrderNo.TabIndex = 1;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(142, 28);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(60, 13);
+            this.lblDescription.TabIndex = 2;
+            this.lblDescription.Text = "Description";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(145, 58);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(233, 20);
+            this.txtDescription.TabIndex = 3;
+            // 
+            // lblInQueue
+            // 
+            this.lblInQueue.AutoSize = true;
+            this.lblInQueue.Location = new System.Drawing.Point(30, 105);
+            this.lblInQueue.Name = "lblInQueue";
+            this.lblInQueue.Size = new System.Drawing.Size(87, 13);
+            this.lblInQueue.TabIndex = 4;
+            this.lblInQueue.Text = "Orders in Queue:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(303, 105);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(222, 105);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.TabIndex = 6;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // lstOrders
+            // 
+            this.lstOrders.FormattingEnabled = true;
+            this.lstOrders.Location = new System.Drawing.Point(397, 58);
+            this.lstOrders.Name = "lstOrders";
+            this.lstOrders.Size = new System.Drawing.Size(212, 121);
+            this.lstOrders.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 262);
+            this.Controls.Add(this.lstOrders);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblInQueue);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.txtOrderNo);
+            this.Controls.Add(this.lblOrderNo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOrderNo;
+        private System.Windows.Forms.TextBox txtOrderNo;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label lblInQueue;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.ListBox lstOrders;
+    }
+}
+
---
+++ OrderForm/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrderForm
+{
+    public partial class Form1 : Form
+    {
+
+        struct Order
+        {
+            private string _orderNo;
+            private string _description;
+
+            public string OrderNo
+            {
+                get { return _orderNo; }
+            }
+
+            public string Description
+            {
+                get { return _description; }
+            }
+
+            public Order(string orderNo, string description)
+            {
+                _orderNo = orderNo;
+                _description = description;
+            }
+        }
+
+        Queue<Order> orders = new Queue<Order>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblInQueue.Text = String.Format("Orders in Queue: {0}", orders.Count);
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            orders.Enqueue(new Order(txtOrderNo.Text, txtDescription.Text));
+            lblInQueue.Text = String.Format("Orders in Queue: {0}", orders.Count);
+
+            lstOrders.Items.Clear();
+            foreach (var order in orders)
+            {
+                lstOrders.Items.Add(String.Format(
+                    "Order [{0}] {1}", order.OrderNo, order.Description));
+            }
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            if (orders.Count.Equals(0)) return;
+            var order = orders.Dequeue();
+            lblInQueue.Text = String.Format("Orders in Queue: {0}", orders.Count);
+
+            MessageBox.Show(String.Format("Order [{0}] {1}",
+                order.OrderNo, order.Description));
+
+            lstOrders.Items.Clear();
+            foreach (var item in orders)
+            {
+                lstOrders.Items.Add(String.Format(
+                    "Order [{0}] {1}", item.OrderNo, item.Description));
+            }
+
+        }
+    }
+}
