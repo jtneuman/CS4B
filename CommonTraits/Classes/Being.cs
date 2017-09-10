60041ca436ef83c256a2bfab5ec69eb3607902b8
Jeff <jeff.neumann@phoenix.edu>
2017-09-10 18:36:57 -0400

    Added Being and TheForce classes to CommonTraits assembly.

---
+++ CommonTraits/Classes/Being.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonTraits.Enums;
+
+namespace CommonTraits.Classes
+{
+    public class Being : TheForce
+    {
+        public string Name { get; private set; }
+        public Races Race { get; set; }
+
+        public Being(string name, Races race, int midichlorians) : base(midichlorians)
+        {
+            Name = name;
+            Race = race;
+        }
+
+        protected sealed override void AddForce(Force force)
+        {
+            if(!Forces.Contains(force))
+            Forces.Add(force);
+        }
+
+        public override string WhatAreYou(Being suspect)
+        {
+            if (suspect == null)
+                return "I don't exist";
+            else if (suspect.Forces.Contains(Force.DarkSide) &&
+                Forces.Contains(Force.DarkSide))
+                return "I'm a Sith";
+            else if (suspect.Forces.Contains(Force.LightSide))
+                return "I'm a Jedi";
+
+            return "I'm neither Sith nor Jedi";
+        }
+
+        public virtual string AreYouSith(Being suspect)
+        {
+            return "I'm not a Sith";
+        }
+    }
+}
