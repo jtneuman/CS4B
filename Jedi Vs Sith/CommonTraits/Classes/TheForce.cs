b4c127a740ce707a1658a1696e492f459379b715
Jeff <jeff.neumann@phoenix.edu>
2017-09-10 19:16:40 -0400

    Initial commit for refactoring of Jedi Vs Sith to a new project folder.

---
+++ Jedi Vs Sith/CommonTraits/Classes/TheForce.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonTraits.Enums;
+
+namespace CommonTraits.Classes
+{
+    public abstract class TheForce
+    {
+        public List<Force> Forces { get; private set; }
+        public int Midichlorians { get; private set; }
+
+        public TheForce(int midichlorians)
+        {
+            Midichlorians = midichlorians;
+            Forces = new List<Force>();
+
+        }
+
+        public abstract string WhatAreYou(Being suspect);
+
+        protected virtual void AddForce(Force force)
+        {
+            Forces.Add(force);
+        }
+    }
+}
---
+++ Jedi Vs Sith/CommonTraits/Enums/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTraits.Enums
+{
+    public enum Races
+    {
+        Human,
+        Gungan,
+        Other
+    }
+
+    public enum Force
+    {
+        DarkSide,
+        LightSide
+    }
+}
