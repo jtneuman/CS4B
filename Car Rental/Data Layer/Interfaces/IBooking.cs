4be60da4c9da47488aa704f280639657646eaa3e
Jeff <jeff.neumann@phoenix.edu>
2017-09-18 07:24:28 -0400

    Added Entity Interfaces and classes through VehicleType.

---
+++ Car Rental/Data Layer/Interfaces/IBooking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Layer.Interfaces
+{
+    public interface IBooking
+    {
+        int Id { get; set; }
+        int VehicleId { get; set; }
+        int CustomerId { get; set; }
+        DateTime Rented { get; set; }
+        DateTime Returned { get; set; }
+        double Cost { get; set; }
+    }
+}
