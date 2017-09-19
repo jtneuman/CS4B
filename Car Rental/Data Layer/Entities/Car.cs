f011c3a5bacdc21bcffc8674c86d33e72ea49341
Jeff <jeff.neumann@phoenix.edu>
2017-09-19 19:44:00 -0400

    Added IVehicle interface and Vehicle and Car classes.

---
+++ Car Rental/Data Layer/Entities/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data_Layer.Interfaces;
+
+namespace Data_Layer.Entities
+{
+    public class Car : Vehicle
+    {
+        public Car()
+        {
+
+        }
+        public Car(IVehicle vehicle) : base(vehicle)
+        {
+
+        }
+    }
+}
---
+++ Car Rental/Data Layer/Interfaces/IVehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Layer.Interfaces
+{
+    public interface IVehicle
+    {
+        int Id { get; set; }
+        int TypeId { get; set; }
+        double BasePricePerKm { get; set; }
+        double BasePricePerDay { get; set; }
+        double DayTariff { get; set; }
+        double KmTariff { get; set; }
+        string RegistrationNumber { get; set; }
+        double Meter { get; set; }
+
+    }
+}
