25007c9e1275a90a46e42a36233873cd3e6df8a5
Jeff <jeff.neumann@phoenix.edu>
2017-09-21 07:38:49 -0400

    Set up form variable and added region in form code behind file.

--- Car Rental/Car Rental/Form1.cs
+++ Car Rental/Car Rental/Form1.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Business_Layer.Classes;
+using Business_Layer.Interfaces;
+using Data_Layer.Data_Layers;
 
 namespace Car_Rental
 {
     public partial class Form1 : Form
     {
+
+        IBookingProcessor processor;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            processor = new BookingProcessor(new CollectionDataLayer());
+        }
+
+
+        #region Fill Data Methods
+
+
+
+        #endregion
+
+        #region Action Methods
+
+
+
+        #endregion
+
+        #region Helper Methods
+
+
+
+        #endregion
+
+        #region Button Events
+
+
+
+        #endregion
+
+
     }
 }
