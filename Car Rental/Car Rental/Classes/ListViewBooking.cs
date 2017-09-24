baecece47a4ac24b7d3d37fbb284e927ff2878b2
Jeff <jeff.neumann@phoenix.edu>
2017-09-24 10:29:26 -0400

    Added ListViewBooking class.

---
+++ Car Rental/Car Rental/Classes/ListViewBooking.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Rental.Classes
+{
+    class ListViewBooking
+    {
+        public int BookingId { get; set; }
+        public int CustomerId { get; set; }
+        public double Cost { get; set; }
+        public string Customer { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string VehicleType { get; set; }
+        public DateTime Rented { get; set; }
+        public DateTime Returned { get; set; }
+
+        public string[] ToArray()
+        {
+            return new string[]
+            {
+                BookingId.ToString(),
+                RegistrationNumber,
+                VehicleType,
+                Customer,
+                Cost.ToString(),
+                Rented.ToString(),
+                Returned == DateTime.MinValue ?
+                    String.Empty : Returned.ToString()
+        };
+        }
+    }
+}
