cd118de88959c687b9df8f0753ffd7565fb35706
Jeff <jeff.neumann@phoenix.edu>
2017-09-28 07:51:15 -0400

    Added WritToFile and ReadFile methods - next to create GUI.

---
+++ ReadandWrite/ReadandWrite/Classes/IO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadandWrite.Classes
+{
+
+    enum DataFormat{
+        Binary,
+        Text
+    }
+
+    class IO
+    {
+        static bool WriteToFile(string path, DataFormat dataFormat, object data)
+        {
+            try
+            {
+                var file = new FileStream(path, FileMode.Create, FileAccess.Write);
+
+                if (dataFormat.Equals(DataFormat.Binary)) { }
+                else if (dataFormat.Equals(DataFormat.Text)) { }
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }// end WriteToFile method
+
+        static bool ReadFile(string path, DataFormat dataFormat, out object data)
+        {
+            data = null;
+
+            try
+            {
+                var file = new FileStream(path, FileMode.Open,
+                    FileAccess.Read);
+                if (dataFormat.Equals(DataFormat.Binary)) { }
+                else if (dataFormat.Equals(DataFormat.Text)) { }
+
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}
