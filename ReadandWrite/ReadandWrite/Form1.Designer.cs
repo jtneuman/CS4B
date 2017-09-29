dae3e7e38bc08779fb0ee85301183c05b447a821
Jeff <jeff.neumann@phoenix.edu>
2017-09-29 06:41:44 -0400

    Added gui for exercise.

--- ReadandWrite/ReadandWrite/Form1.Designer.cs
+++ ReadandWrite/ReadandWrite/Form1.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.RichTextBox();
+            this.rbnBinary = new System.Windows.Forms.RadioButton();
+            this.rbnText = new System.Windows.Forms.RadioButton();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Message";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(28, 59);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(361, 96);
+            this.txtMessage.TabIndex = 1;
+            this.txtMessage.Text = "";
+            // 
+            // rbnBinary
+            // 
+            this.rbnBinary.AutoSize = true;
+            this.rbnBinary.Location = new System.Drawing.Point(28, 175);
+            this.rbnBinary.Name = "rbnBinary";
+            this.rbnBinary.Size = new System.Drawing.Size(80, 17);
+            this.rbnBinary.TabIndex = 2;
+            this.rbnBinary.TabStop = true;
+            this.rbnBinary.Text = "Binary Data";
+            this.rbnBinary.UseVisualStyleBackColor = true;
+            // 
+            // rbnText
+            // 
+            this.rbnText.AutoSize = true;
+            this.rbnText.Location = new System.Drawing.Point(28, 215);
+            this.rbnText.Name = "rbnText";
+            this.rbnText.Size = new System.Drawing.Size(72, 17);
+            this.rbnText.TabIndex = 3;
+            this.rbnText.TabStop = true;
+            this.rbnText.Text = "Text Data";
+            this.rbnText.UseVisualStyleBackColor = true;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(146, 175);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(90, 57);
+            this.btnRead.TabIndex = 4;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(286, 175);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(102, 57);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 261);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.rbnText);
+            this.Controls.Add(this.rbnBinary);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox txtMessage;
+        private System.Windows.Forms.RadioButton rbnBinary;
+        private System.Windows.Forms.RadioButton rbnText;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
