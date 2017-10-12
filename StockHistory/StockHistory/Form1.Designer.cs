a94a194fa30aa0643c46bea05e25a151b122afc6
Jeff <jeff.neumann@phoenix.edu>
2017-10-12 07:24:44 -0400

    Gui part 1 complete - next StockFactory class.

--- StockHistory/StockHistory/Form1.Designer.cs
+++ StockHistory/StockHistory/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TxtResult = new System.Windows.Forms.RichTextBox();
+            this.BtnGetStockData = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(12, 12);
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(399, 346);
+            this.TxtResult.TabIndex = 0;
+            this.TxtResult.Text = "";
+            // 
+            // BtnGetStockData
+            // 
+            this.BtnGetStockData.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnGetStockData.Location = new System.Drawing.Point(265, 364);
+            this.BtnGetStockData.Name = "BtnGetStockData";
+            this.BtnGetStockData.Size = new System.Drawing.Size(145, 39);
+            this.BtnGetStockData.TabIndex = 1;
+            this.BtnGetStockData.Text = "Get Stock Data";
+            this.BtnGetStockData.UseVisualStyleBackColor = true;
+            this.BtnGetStockData.Click += new System.EventHandler(this.BtnGetStockData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 417);
+            this.Controls.Add(this.BtnGetStockData);
+            this.Controls.Add(this.TxtResult);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox TxtResult;
+        private System.Windows.Forms.Button BtnGetStockData;
     }
 }
 
--- StockHistory/StockHistory/Form1.cs
+++ StockHistory/StockHistory/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using StockHistory.Classes;
+using System.Threading;
 
 namespace StockHistory
 {
@@ -16,5 +18,50 @@ namespace StockHistory
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Methods
+
+        private List<Stock> GetData(List<string> symbols,
+            int yearsOfHistory = 1)
+        {
+            Thread.Sleep(2000);
+            return null;
+        }
+
+        private string FormatOutput(List<Stock> stocks)
+        {
+            Thread.Sleep(2000);
+            return null;
+        }
+
+        #endregion
+
+
+
+        private async void BtnGetStockData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BtnGetStockData.Enabled = false;
+                TxtResult.Text = String.Empty;
+
+                var symbols = new List<string> { "msft", "intc" };
+                var stocks = await Task.Run(() => GetData(symbols, 2));
+                var output = await Task.Run(() => FormatOutput(stocks));
+                
+                TxtResult.Text = output;
+                BtnGetStockData.Enabled = true;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }// end BtnGetStockData click event
     }
 }
